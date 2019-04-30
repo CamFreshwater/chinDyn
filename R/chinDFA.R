3c3e871c320b1b7dff01732325f363dc8a6a0860
Cameron Freshwater <ccfreshwater@gmail.com>
2019-04-30 15:05:45 -0700

    DFA model selection for SS pops

--- R/chinDFA.R
+++ R/chinDFA.R
@@ -3,7 +3,9 @@
 # Script to fit DFAs; output visualized in chinDFAOutput.Rmd
 # -----
 
-require(here); require(MARSS); require(tidyverse); require(ggplot2)
+listOfPackages <- c("here", "MARSS", "tidyverse", "ggplot2", "parallel", 
+                    "doParallel", "foreach", "tictoc")
+lapply(listOfPackages, require, character.only = TRUE)
 
 byDat <- read.csv(here("data/salmonData/CLEANcwtInd_age2SR_BY.csv"), 
                   stringsAsFactors = FALSE)
@@ -12,7 +14,7 @@ eyDat <- read.csv(here("data/salmonData/CLEANcwtInd_age2SR_OEY.csv"),
 
 #focus on subset of BC pops for initial analyses
 byDatTrim <- byDat %>% 
-  filter(region %in% c("LFR", "MFR", "UFR"))
+  filter(region %in% c("LFR", "MFR", "UFR", "ECVI", "SPGSD", "NPGSD"))
   
 #Convert to matrices (necessary for DFA)
 byMat <- byDatTrim %>%
@@ -31,7 +33,51 @@ nYrs <- ncol(byMat)
 byMatZ <- MARSS::zscore(byMat)
 
 #preliminary model fit test
-modelList <- list(m = 2, R = "diagonal and unequal")
-cntrList <- list(maxit = 1000)
+modelList <- list(m = 2, R = "diagonal and equal")
+cntrList <- list(maxit = 200)
 dfaTest <- MARSS(byMatZ, model = modelList, z.score = TRUE, form = "dfa", 
-                 control = cntrList)
+                 control = cntrList, method = "BFGS-kf")
+
+
+## Function to fit DFA and save output data
+fitDFA <- function(mat, inR, inM, maxIteration = 500) {
+  cntrList <- list(minit = 100, maxit = maxIteration)
+  dfaModel <- list(A = "zero", R = inR, m = inM)
+  fitMod <- MARSS(byMatZ, model = dfaModel, control = cntrList,
+                 form = "dfa", z.score  = TRUE)
+  #If faster algorithm fails to converge use alternative
+  while(fitMod$convergence != 0) {
+    fitMod <- MARSS(byMatZ, model = dfaModel, control = list(maxit = 4000),
+                    inits = as.matrix(coef(fitMod)[1]), form = "dfa", 
+                    z.score  = TRUE, method = "BFGS-kf")
+  }
+  outName <- paste("fitMod", inM, rapportools::tocamel(inR), "rds", sep=".")
+  saveRDS(fitMod, here::here("data", "dfaFits", outName))
+}
+
+
+## Fit models in parallel using multiple cores
+inRSeq <- c("diagonal and equal", "diagonal and unequal", "equalvarcov",
+            "unconstrained")
+inMList <- list(2, 3, 4, 5, 6, 7)
+for (i in seq_along(inRSeq)) {
+  Ncores <- detectCores()
+  inRDum <- inRSeq[i]
+  cl <- makeCluster(Ncores - 2) #save two cores
+  registerDoParallel(cl)
+  clusterEvalQ(cl, c(library(MARSS), library(here), library(Rcpp), 
+                     library(RcppArmadillo)))
+  clusterExport(cl, c("byMatZ", "inRDum", "inMList", "fitDFA"), 
+                envir=environment())
+  tic("run in parallel")
+  parLapply(cl, inMList, function(x) {
+    fitDFA(byMatZ, inR = inRDum, inM = x, maxIteration = 1000)
+  })
+  stopCluster(cl) #end cluster
+  toc()
+}
+
+fitDFA(byMatZ, inR = "diagonal and equal", inM = 2, maxIteration = 500)
+
+
+tt <- readRDS(here("data", "dfaFits", outName))

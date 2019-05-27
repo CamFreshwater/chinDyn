c55b3bc33ca123e7ea2a0c1aecbf8890704ad644
Cameron Freshwater <ccfreshwater@gmail.com>
2019-05-27 13:40:22 -0700

    update map for pres

--- R/survMap.R
+++ R/survMap.R
@@ -18,14 +18,30 @@ stockDat <- read.csv("C:/github/chinDyn/data/salmonData/CLEANcwtInd_age2SR_OEY.c
                          "WCVI")) ~ "south",
            TRUE ~ "SS"
          )) %>% 
-  mutate(grp = paste(smoltType, aggReg, sep = "_"))
+  mutate(grp = as.factor(paste(smoltType, aggReg, sep = "_"))) %>% 
+  arrange(desc(lat)) %>% 
+  mutate(grp = factor(grp, unique(grp))) %>% 
+  filter(!grp %in% c("oceantype_north", "streamtype_south")) %>% 
+  mutate(grp = fct_recode(grp, "Yearling\nNorth" = "streamtype_north", 
+                          "Yearling\nSalish Sea" = "streamtype_SS", 
+                          "Subyearling\nSalish Sea" = "oceantype_SS",
+                          "Subyearling\nSouth" = "oceantype_south"))
 
 na <- map_data("world") %>% 
   filter(region %in% c("Canada", "USA"))
 
-ggplot(data = na, mapping = aes(x = long, y = lat, group = group)) + 
+mapP <- ggplot(data = na, mapping = aes(x = long, y = lat, group = group)) + 
   coord_fixed(xlim = c(-136, -119), ylim = c(42.5, 59.5), ratio = 1.3) + 
-  geom_polygon(color = "black", fill = "gray") +
+  geom_polygon(color = "black", fill = "gray80") +
   geom_jitter(data = stockDat, aes(x = long, y = lat, color = as.factor(grp)), 
-             inherit.aes = FALSE, width = 0.25, height = 0.5)
+             inherit.aes = FALSE, width = 0.1, height = 0.1) + 
+  labs(color = "Life History Grouping", x = "Longitude", y = "Latitude") +
+  samSim::theme_sleekX(legendSize = 0.8) +
+  theme(strip.text.x = element_blank(),
+        strip.background = element_rect(colour="white", fill="white"),
+        legend.position=c(0.2, 0.15))
 
+png(here::here("figs", "hatcheryMap.png"), height = 5.5, width = 6, units = "in",
+    res = 300)
+print(mapP)
+dev.off()

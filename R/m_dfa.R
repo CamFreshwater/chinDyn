3b5b3719c289705d71bcb5afb630df0b81becc45
Cameron Freshwater <ccfreshwater@gmail.com>
2021-03-16 13:50:50 -0700

    remove figure plotting from dfa fitting

--- R/m_dfa.R
+++ R/m_dfa.R
@@ -271,180 +271,44 @@ map(dfa_fits, function (x) {
 })
 
 
-## MAKE PLOTS ------------------------------------------------------------------
-
-source(here::here("R", "functions", "plot_fitted_bayes.R"))
-
-
-# predicted fits
-pred_list <- pmap(list(dfa_fits, surv_tbl$names, surv_tbl$years), 
-                  fitted_preds,
-                  descend_order = TRUE)
-# scale colors based on observed range over entire dataset
-col_ramp_in <- pred_list %>% 
-  bind_rows() %>% 
-  pull(last_mean) %>% 
-  range()
-
-fit_list <- map(pred_list, plot_fitted_pred, col_ramp = col_ramp_in, facet_col = 5)
-
-mort_fit_panel <- cowplot::plot_grid(
-  fit_list[[1]], fit_list[[2]], fit_list[[3]], fit_list[[4]], fit_list[[5]],
-  axis = c("r"), align = "v", 
-  rel_heights = c(2/11, 3/11, 1/11, 2/11, 3/11),
-  ncol=1 
-)
-
-
-rot_list <- map(dfa_fits, rotate_trends)
-
-# make plots 
-trend_list <- pmap(list(rot_list, surv_tbl$years), 
-                   function(x, obs_years) {
-                     plot_trends(x, years = obs_years) +
-                       scale_x_continuous(breaks = c(1970, 1990, 2010), 
-                                          limits = c(min(surv$year), 
-                                                     max(surv$year)))
-                   }
-)
-
-
-loadings_list <- pmap(list(rot_list, surv_tbl$names), 
-                      function(x, names) {
-                        plot_loadings(x, names = names$stock_name) +
-                          lims(y = c(-2, 2))
-                      }
-) 
-
-fig_path <- paste("figs", "dfa", "bayes", "mortality", sep = "/")
-
-pdf(here::here(fig_path, "fits.pdf"))
-fit_list
-dev.off()
-
-pdf(here::here(fig_path, "trends.pdf"))
-trend_list
-dev.off()
-
-pdf(here::here(fig_path, "loadings.pdf"))
-loadings_list
-dev.off()
-
-
-# Make 4 panel plot of dominant trends by juvenile grouping
-rot_dat <- pmap(list(rot_list, kept_grps$j_group3, surv_tbl$years), 
-     function(x, y, z) {
-  data.frame(
-    mean = c(x$trends_mean[1, ], x$trends_mean[2, ]),
-    lo = c(x$trends_lower[1, ], x$trends_lower[2, ]),
-    hi = c(x$trends_upper[1, ], x$trends_upper[2, ]),
-    trend = rep(c("trend1", "trend2"), each = length(z)),
-    j_group = y,
-    years = z
-  )
-}) %>% 
-  bind_rows() %>% 
-  left_join(., surv %>% select(smolt, j_group = j_group3) %>% distinct, 
-            by = "j_group") %>% 
-  glimpse()
-
-trends <- rot_dat %>% 
-  filter(trend == "trend1") %>% 
-  ggplot(., aes(x = years, y = mean, colour = smolt)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = lo, ymax = hi, fill = smolt), alpha = 0.3) +
-  facet_wrap(~j_group) +
-  ggsidekick::theme_sleek() +
-  geom_hline(yintercept = 0, linetype = 2) +
-  labs(x = "Brood Year", y = "Shared Trend") +
-  theme(legend.position = "top")
-
-png(here::here("figs", "dfa", "bayes", "mortality", "trend_all_groups.png"), 
-    height = 5.5, width = 7.5, res = 300, units = "in")
-trends
-dev.off()
-
-
-## DFA model selection approach (NOT USED) -------------------------------------
-# 
-# # specify z matrix structure
-# z_mm <- model.matrix(~ 0 + smolt, 
-#                    data = stk_tbl %>% 
-#                      filter(stock %in% keep_stks$stock))
-# zmat_list <- vector(mode = "list", length = length(z_mm))
-# counter <- 0
-# for (j in seq_len(nrow(z_mm))) {
-#   for (i in seq_len(ncol(z_mm))) {
-#     counter <- counter + 1
-#     if (z_mm[j, i] == 1) {
-#       zmat_list[[counter]] <- paste("z", i, sep = "")
-#     } else {
-#       zmat_list[[counter]] <- 0
-#     }
-#   }
-# }
-# zmat <- matrix(zmat_list, nrow = n_ts, ncol = ncol(z_mm), byrow = TRUE)
-# 
-# # specify other MARSS parameters
-# U <- x0 <- A <- "zero"
-# Q <- B <- "identity"
-# R <- "diagonal and unequal"
-# V0 <- diag(5, ncol(zmat))
-# 
-# # z-score input matrix
-# m_mat_z <- zscore(m_mat)
-# 
-# model.list <- list(A=A, U=U, x0=x0, Q=Q, B=B,
-#                    R=R, Z=zmat, V0=V0)
-# kemTestMod <- MARSS(m_mat_z, model = model.list, 
-#                     control=list(minit=1000,maxit=5000, trace=1,
-#                                  conv.test.slope.tol=0.1))
-# while(kemTestMod$convergence!=0){
-#   fit <- MARSS(m_mat_z, 
-#                model=fit.model, 
-#                control=list(maxit=4000, trace=1),
-#                inits=as.matrix(coef(fit)[1]),
-#                method="BFGS")
-# }
-
-
+#-------------------------------------------------------------------------------
 
 ## MODEL SELCTION FOR DFA STRUCTURE
 
-m_mat_tibble <- tibble(group = levels(surv$j_group3b)) %>%
-  mutate(
-    m_mat = surv %>%
-      filter(!is.na(M)) %>%
-      group_split(j_group3b) %>%
-      map(., make_mat)
-  )
-surv_tbl <- expand.grid(group = levels(surv$j_group3b),
-                        m = c(1, 2),
-                        est_ar = c(0, 1),
-                        est_ma = c(0, 1),
-                        est_nu = c(0, 1)) %>%
-  as_tibble() %>%
-  left_join(., m_mat_tibble, by = "group") %>%
-  filter(group %in% kept_grps$j_group3b,
-         group == "puget_oceantype",
-         m == "2")
-
-test_fits <- furrr::future_pmap(
-    list(surv_tbl$m_mat, surv_tbl$m, surv_tbl$group,
-         surv_tbl$est_ar,
-         surv_tbl$est_ma, surv_tbl$est_nu),
-    .f = function(y, m, z,
-                  est_ar, est_ma, est_nu) {
-      dfa_fits = fit_dfa(y = y, num_trends = m, estimate_nu = est_nu,
-                         estimate_trend_ar = est_ar, estimate_trend_ma = est_ma,
-                         zscore = FALSE, #only center don't scale
-                         iter = 4000, chains = 4, thin = 1,
-                         control = list(adapt_delta = 0.99, max_treedepth = 20))
-    },
-    .progress = TRUE,
-    .options = furrr::furrr_options(seed = TRUE)
-  )
-
-loo_list <- map(test_fits, bayesdfa::loo)
-surv_tbl$looic <- map(loo_list, function(x) x$estimates["looic", "Estimate"]) %>%
-  unlist()
+# m_mat_tibble <- tibble(group = levels(surv$j_group3b)) %>%
+#   mutate(
+#     m_mat = surv %>%
+#       filter(!is.na(M)) %>%
+#       group_split(j_group3b) %>%
+#       map(., make_mat)
+#   )
+# surv_tbl <- expand.grid(group = levels(surv$j_group3b),
+#                         m = c(1, 2),
+#                         est_ar = c(0, 1),
+#                         est_ma = c(0, 1),
+#                         est_nu = c(0, 1)) %>%
+#   as_tibble() %>%
+#   left_join(., m_mat_tibble, by = "group") %>%
+#   filter(group %in% kept_grps$j_group3b,
+#          group == "puget_oceantype",
+#          m == "2")
+# 
+# test_fits <- furrr::future_pmap(
+#     list(surv_tbl$m_mat, surv_tbl$m, surv_tbl$group,
+#          surv_tbl$est_ar,
+#          surv_tbl$est_ma, surv_tbl$est_nu),
+#     .f = function(y, m, z,
+#                   est_ar, est_ma, est_nu) {
+#       dfa_fits = fit_dfa(y = y, num_trends = m, estimate_nu = est_nu,
+#                          estimate_trend_ar = est_ar, estimate_trend_ma = est_ma,
+#                          zscore = FALSE, #only center don't scale
+#                          iter = 4000, chains = 4, thin = 1,
+#                          control = list(adapt_delta = 0.99, max_treedepth = 20))
+#     },
+#     .progress = TRUE,
+#     .options = furrr::furrr_options(seed = TRUE)
+#   )
+# 
+# loo_list <- map(test_fits, bayesdfa::loo)
+# surv_tbl$looic <- map(loo_list, function(x) x$estimates["looic", "Estimate"]) %>%
+#   unlist()

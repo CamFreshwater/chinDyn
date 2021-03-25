ae99211e62fb52490b62ccf2283398be2815b6fd
CamFreshwater <ccfreshwater@gmail.com>
2021-03-25 16:39:32 -0700

    update conv issues

--- R/convergence_issues_dfa.R
+++ R/convergence_issues_dfa.R
@@ -1,14 +1,10 @@
 ## Non-convergence Example
-# DFA model that throws non-convergence errors when data are centered/scaled, but
-# not when centered.
+# DFA model that throws non-convergence errors when unique observation variance 
+# is estimated for each time series. Otherwise ok. 
 # Input is time series of average age at maturity for stocks of Pacific salmon 
-# returning to Puget Sound. Similar datasets (i.e. mean age of stocks returning
-# to other regions) converge well.
-# NOTE: I'm now taking your advice and fitting a model of maximum complexity
-# that estimates nu and AR/MA parameters. No convergence issues with centered
-# data there either.
+# returning to Puget Sound. Some other similar datasets (i.e. mean age of stocks
+# returning to other regions) converge with this model, others don't.
 
-library(MARSS)
 library(tidyverse)
 library(bayesdfa)
 library(rstan)
@@ -19,23 +15,29 @@ options(mc.cores = parallel::detectCores())
 
 dat <- readRDS(here::here("data", "salmonData", 
                               "cwt_indicator_surv_clean.RDS")) %>%  
-  filter(j_group3b == "puget_oceantype") %>% 
+  filter(j_group3b == "sog_oceantype", 
+         !is.na(gen_length)) %>% 
   droplevels()
 
 mat1 <- dat %>%
-  select(year, stock, M) %>%
-  spread(key = stock, value = M) %>%
+  select(year, stock, gen_length) %>%
+  spread(key = stock, value = gen_length) %>%
   as.matrix() 
 mat_in <- t(mat1[, 2:ncol(mat1)])
 colnames(mat_in) <- mat1[, "year"]
 
+var_seq <- seq(1, nrow(mat_in), by = 1)
 
-fit_scale <- fit_dfa(
-  y = mat_in, num_trends = 2, zscore = TRUE,
+# fit model with diagonal and unequal R matrix
+fit_unequal <- fit_dfa(
+  y = mat_in, num_trends = 2, zscore = FALSE,
+  varIndx = var_seq,
   iter = 3000, chains = 4, thin = 1,
   control = list(adapt_delta = 0.99, max_treedepth = 20)
 )
-fit_center <- fit_dfa(
+
+# fit model with diagonal and equal R matrix
+fit_diag <- fit_dfa(
   y = mat_in, num_trends = 2, zscore = FALSE,
   iter = 3000, chains = 4, thin = 1,
   control = list(adapt_delta = 0.99, max_treedepth = 20)

f8cb4b737cc37279d6c83489227852b0017978ca
Cameron Freshwater <ccfreshwater@gmail.com>
2021-08-03 12:21:09 -0700

    update map with CP's hatchery lat/longs

--- R/hatchery_map.R
+++ R/hatchery_map.R
@@ -35,25 +35,6 @@ dat <- read.csv(here::here("data", "salmon_data", "metadata_clean.csv")) %>%
     juv_region = fct_recode(juv_region, 
                             "North" = "north", "Puget Sound" = "puget",
                             "Strait of Georgia" = "sog", "South" = "south")
-    # grouping = fct_recode(j_group3b,
-    #                       "North Subyearling" = "north_oceantype", 
-    #                       "North Yearling" = "north_streamtype", 
-    #                       "Puget Subyearling" = "puget_oceantype", 
-    #                       "Puget Yearling" = "puget_streamtype", 
-    #                       "SoG Subyearling" = "sog_oceantype", 
-    #                       "SoG Yearling" = "sog_streamtype",
-    #                       "South Subyearling" = "south_oceantype",
-    #                       "South Yearling" = "south_streamtype"
-    # ),
-    # grouping = fct_relevel(grouping,
-    #                        "North Subyearling", 
-    #                        "North Yearling", 
-    #                        "Puget Subyearling", 
-    #                        "Puget Yearling", 
-    #                        "SoG Subyearling", 
-    #                        "SoG Yearling",
-    #                        "South Subyearling",
-    #                        "South Yearling")
   ) %>% 
   filter(!stock %in% c("TST", "AKS"))
 
@@ -65,7 +46,7 @@ shape_pal <- c(21, 23)
 names(shape_pal) <- c("Subyearling", "Yearling")
 
 oey_map <- base_map +
-  geom_jitter(data = dat,
+  geom_jitter(data = dat, #%>% filter(stock %in% c("HOK", "ELK", "QUE", "SOO", "SRH")),
              aes(x = hatch_long, y = hatch_lat, 
                  fill = juv_region, shape = life_history),
              height = 0.25, width = 0.25, size = 3,
@@ -77,31 +58,33 @@ oey_map <- base_map +
   guides(fill = guide_legend(override.aes = list(shape = 21))) +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank())
-
+oey_map
 
 # inset map
 w_can <- map_data("world", region = c("usa", "canada")) %>%
   filter(lat > 25 & lat < 80,
          long >-175 & long < -60) %>% 
   fortify(.)
-inset_map <-  ggplot() +
+inset_map <- ggplot() +
   geom_polygon(data = w_can, mapping = aes(x = long, y = lat, group = group), 
                color = "black", fill = "darkgrey") + 
   labs(x = "", y = "") +
-  ggsidekick::theme_sleek() +
+  # ggsidekick::theme_sleek() +
   geom_rect(aes(xmin = -136.5, xmax = -118, ymin = 42, ymax = 60),
             fill = NA, lty = 2, col = "black") +
+  theme_linedraw() +
   theme(strip.background = element_rect(colour="white", fill="white"),
         legend.position = "top",
         axis.title = element_blank(),
         axis.ticks = element_blank(),
         axis.text = element_blank()) +
-  coord_fixed(ratio = 1.3)
+  coord_map("azequalarea", orientation = c(60, -140, 20))
+  # coord_fixed(ratio = 1.3)
 
 
 png(here::here("figs", "ms_figs", "stock_locs.png"), 
     height = 7, width = 6, units = "in", res = 300)
-cowplot::ggdraw() +
-  cowplot::draw_plot(oey_map) +
-  cowplot::draw_plot(inset_map, x = 0.09, y = 0, width = 0.35, height = 0.4)
+cowplot::ggdraw(oey_map) +
+  # cowplot::draw_plot() +
+  cowplot::draw_plot(inset_map, x = 0.09, y = 0.01, width = 0.35, height = 0.35)
 dev.off()
